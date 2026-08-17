296d2588915afdf9580f463ca46b2328e038a988
Alina Mikhailova <alina.mik.kz@gmail.com>
2026-08-17 10:14:30 +1000

    feat(application): add in-memory store

--- internal/application/application.go
+++ internal/application/application.go
@@ -9,3 +9,25 @@ type Application struct {
 	Term int `json:"term"`
 	Status string `json:"status"`
 }
+
+// Store represents a simple memory cell
+type Store struct {
+	items []Application
+	nextID int
+}
+
+// NewStore create and returns an exemplar of Store
+func NewStore() *Store {
+	return &Store{nextID: 1}
+}
+
+// Add accept the request and saves it to memory, returning the completed request
+func (s *Store) Add(app Application) Application { // WHY: `*` is used so that the original is modified, rather than a copy
+	app.ID = s.nextID
+	app.Status = "new"
+
+	s.nextID++ // NOTE: shift the counter
+	s.items = append(s.items, app) // NOTE: place in storage
+
+	return app
+}
